03ce0ebdff96473047af3282615a4aa6a6f2dc8a
anubhav.mishra <anubhav.mishra@appventurez.com>
2024-08-16 20:46:05 +0530

    url updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import { redirect } from "next/navigation";
 export default function Home() {
   // redirect("/main/home");
 
-  redirect("http://localhost:3129/login");
+  // redirect("http://localhost:3129/login");
+  redirect("https://worms-app.onrender.com/login");
   // return null;
   // return <></>
 }
